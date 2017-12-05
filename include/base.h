2dc985ef74092d41d1951c0d073b4be214a39321
user <user@ebj1870.local>
2017-12-05 14:05:44 +0800

    mv base.h to include

---
+++ include/base.h
@@ -0,0 +1,25 @@
+/***************************************************
+ Xingwei_He All Rights Reserved.
+ * File Name:base.h
+ * Author:
+ * Mail:
+ * Created Time:星期一 12/ 4 11:08:01 2017
+ ***************************************************/
+#ifndef _BASE_H
+#define _BASE_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+
+//Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+#endif
+
