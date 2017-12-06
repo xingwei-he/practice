4258293cebcad02a63bcd24f20c1e02abf7f044f
user <user@ebj1870.local>
2017-12-06 23:05:24 +0800

    vector<vector<int> > levelOrderBottom(TreeNode * root) {

---
+++ lintcode/70_Binary_Tree_Level_Order_Traversal_II.cc
@@ -0,0 +1,62 @@
+#include "practice/include/base.h"
+
+using namespace std;
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+  /*
+   * @param root: A tree
+   * @return: buttom-up level order a list of lists of integer
+   */
+  vector<vector<int> > levelOrderBottom(TreeNode * root) {
+    vector<vector<int> > res;
+    if (nullptr == root) {
+      return res;
+    }
+
+    TreeNode* ptr = root;
+    queue<TreeNode*> node_queue;
+    node_queue.push(ptr);
+    stack<vector<int> > level_stack;
+    while (!node_queue.empty()) {
+      vector<int> temp_vec;
+      int queue_size = node_queue.size();// 当前队列中的节点便是一层的节点
+      for (int i = 0; i < queue_size; i++) {
+	// 每次循环结束都完成一层的遍历
+	ptr = node_queue.front();
+	node_queue.pop();
+	if (nullptr != ptr->left) {
+	  node_queue.push(ptr->left);
+	}
+	if (nullptr != ptr->right) {
+	  node_queue.push(ptr->right);
+	}
+	temp_vec.push_back(ptr->val);
+      }
+      level_stack.push(temp_vec);
+    }
+
+    while (!level_stack.empty()) {
+      res.push_back(level_stack.top());
+      level_stack.pop();
+    }
+    return res;
+  }
+};
+
+int main() {
+  return 0;
+}
