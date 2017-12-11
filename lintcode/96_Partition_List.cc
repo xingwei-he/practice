93b2585f87d554f48aca0481d4170aa68745a24c
user <user@ebj1870.local>
2017-12-11 17:51:08 +0800

    96_Partition_List

---
+++ lintcode/96_Partition_List.cc
@@ -0,0 +1,84 @@
+#include "practice/include/base.h"
+
+using namespace std;
+
+/**
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+
+ * Example
+ * Given 1->4->3->2->5->2->null and x = 3,
+ * return 1->2->2->4->3->5->null.
+ */
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+  /*
+   * @param head: The first node of linked list
+   * @param x: An integer
+   * @return: A ListNode
+   */
+
+  ListNode* splitList(ListNode* head, int pivot) {
+    if (nullptr == head) {
+      return nullptr;
+    }
+    ListNode* temp = new ListNode(-1);
+    temp->next = head;
+    ListNode* left = temp;
+    ListNode* right = temp;
+    while (nullptr != right->next) {
+      while (nullptr != left->next && left->next->val < pivot) {
+	left = left->next;
+      }
+      right = left;
+      while (nullptr != right->next && right->next->val >= pivot) {
+	right = right->next;
+      }
+      //cout << "left:" << left->val << " right:" << right->val << endl;
+      if (left == right || nullptr == left->next || nullptr == right->next) {
+	break;
+      }
+      // 将 right 后面的节点插入到 left 后面
+      ListNode* t = right->next;
+      right->next = t->next;
+      t->next = left->next;
+      left->next = t;
+    }
+    ListNode* h = temp->next;
+    delete temp;
+    temp = nullptr;
+    return h;
+  }
+
+  ListNode * partition(ListNode * head, int x) {
+    return splitList(head, x);
+  }
+};
+
+int main() {
+  ListNode* head = new ListNode(1);
+  ListNode* res = nullptr;
+  Solution sl;
+  res = sl.partition(head, 0);
+  ListNode* ptr = res;
+  while (ptr != nullptr) {
+    cout << ptr->val << "  ";
+    ptr = ptr->next;
+  }
+  cout << endl;
+  return 0;
+}
