ccf3d1f49bd8bef08f6bf37c3e2a1edb8460c7d3
xingwei <120977876@qq.com>
2018-01-07 17:37:34 +0800

    3_Digit_Counts

---
+++ lintcode/3_Digit_Counts.cc
@@ -0,0 +1,62 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:3_Digit_Counts.cc
+ * Author:
+ * Mail:
+ * Created Time:2018年01月07日 星期日 11时50分51秒
+ ***************************************************/
+#include <iostream>
+
+#include "practice/include/base.h"
+
+using namespace std;
+
+/**
+ * Count the number of k's between 0 and n. k can be 0 - 9.
+ *
+ * Example
+ * if n = 12, k = 1 in
+ * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+ * we have FIVE 1's (1, 10, 11, 12)
+ */
+
+class Solution {
+public:
+  /*
+   * @param : An integer
+   * @param : An integer
+   * @return: An integer denote the count of digit k in 1..n
+   */
+  // 方法一 穷举法
+  int countK(const int& n, const int& k) {
+    int temp = n;
+    int counter = 0;
+    do {
+      if (temp % 10 == k) {
+	counter++;
+      }
+      temp /= 10;
+    } while (0 != temp);
+    return counter;
+  }
+
+  int digitCounts(int k, int n) {
+    int counter = 0;
+    for (int i = 0; i <= n; i++) {
+      counter += countK(i, k);
+    }
+    return counter;
+  }
+  // 方法二 利用规律
+};
+
+int main() {
+  int n = 359;
+  int k = 2;
+  cout << "n:" << n << "\tk:" << k << endl; 
+  Solution sl;
+  int res = sl.digitCounts(k, n);
+  cout << "res:" << res << endl;
+  return 0;
+}
+
