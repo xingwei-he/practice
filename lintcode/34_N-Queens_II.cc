51273dceae2ba88e1ac662f868ccd4b0f9c5260e
xingwei.he <xingwei.he@corp.elong.com>
2018-01-09 19:17:33 +0800

    Queens_II

---
+++ lintcode/34_N-Queens_II.cc
@@ -0,0 +1,35 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:34_N-Queens_II.cc
+ * Author:
+ * Mail:
+ * Created Time:星期二  1/ 9 19:14:43 2018
+ ***************************************************/
+#include <iostream>
+
+#include "practice/include/base.h"
+
+using namespace std;
+
+/**
+ * Follow up for N-Queens problem.
+ * Now, instead outputting board configurations, return the total number of distinct solutions.
+ *
+ * Example
+ * For n=4, there are 2 distinct solutions.
+ */
+
+class Solution {
+public:
+  /*
+   * @param n: The number of queens.
+   * @return: The total number of distinct solutions.
+   */
+  int totalNQueens(int n) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
