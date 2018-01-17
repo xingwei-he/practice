6f0de76d8299d9e61ca61b1714edc5c544407a53
xingwei.he <xingwei.he@corp.elong.com>
2018-01-17 19:06:13 +0800

    todo 65

---
+++ lintcode/65_Median_of_two_Sorted_Arrays.cc
@@ -0,0 +1,36 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:65_Median_of_two_Sorted_Arrays.cc
+ * Author:
+ * Mail:
+ * Created Time:星期三  1/17 18:39:14 2018
+ ***************************************************/
+#include <iostream>
+
+#include "practice/include/base.h"
+
+using namespace std;
+
+/**
+ * There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays.
+ *
+ * Example
+ * Given A=[1,2,3,4,5,6] and B=[2,3,4,5], the median is 3.5.
+ * Given A=[1,2,3] and B=[4,5], the median is 3.
+ */
+
+class Solution {
+public:
+  /*
+   * @param A: An integer array
+   * @param B: An integer array
+   * @return: a double whose format is *.5 or *.0
+   */
+  double findMedianSortedArrays(vector<int> &A, vector<int> &B) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
