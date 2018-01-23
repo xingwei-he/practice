8968a3035f1ee16865025dfbd8b5b008516cf974
xingwei.he <xingwei.he@corp.elong.com>
2018-01-23 10:42:33 +0800

    99_Reorder_List

--- lintcode/99_Reorder_List.cc
+++ lintcode/99_Reorder_List.cc
@@ -3,6 +3,14 @@
 using namespace std;
 
 /**
+ * Given a singly linked list L: L0 → L1 → … → Ln-1 → Ln
+ * reorder it to: L0 → Ln → L1 → Ln-1 → L2 → Ln-2 → …
+ *
+ * Example
+ * Given 1->2->3->4->null, reorder it to 1->4->2->3->null.
+ */
+
+/**
  * Definition of ListNode
  * class ListNode {
  * public:
@@ -14,7 +22,6 @@ using namespace std;
  *     }
  * }
  */
-
 
 class Solution {
 public:
