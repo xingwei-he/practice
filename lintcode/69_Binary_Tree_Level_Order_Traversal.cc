755171be166b618c973650b1e44564fb1779f756
xingwei.he <xingwei.he@corp.elong.com>
2018-01-23 16:46:27 +0800

    69_Binary_Tree_Level_Order_Traversal

--- lintcode/69_Binary_Tree_Level_Order_Traversal.cc
+++ lintcode/69_Binary_Tree_Level_Order_Traversal.cc
@@ -17,12 +17,12 @@ using namespace std;
  * Example
  * Given binary tree {3,9,20,#,#,15,7},
  *
- *         3
- *        / \
- *       9  20
- *         /  \
- *        15   7
- *       	   
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *    	   
  * return its level order traversal as:
  * [
  *   [3],
