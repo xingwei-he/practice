95d40f04d20e16c466547da0ca1ebcb6fd54e308
xingwei.he <xingwei.he@corp.elong.com>
2018-01-23 16:42:55 +0800

    70_Binary_Tree_Level_Order_Traversal_II

--- lintcode/70_Binary_Tree_Level_Order_Traversal_II.cc
+++ lintcode/70_Binary_Tree_Level_Order_Traversal_II.cc
@@ -3,6 +3,26 @@
 using namespace std;
 
 /**
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ *
+ * Example
+ * Given binary tree {3,9,20,#,#,15,7},
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *   
+ * return its bottom-up level order traversal as:
+ * [
+ *    [15,7],
+ *    [9,20],
+ *    [3]
+ * ]
+ */
+
+/**
  * Definition of TreeNode:
  * class TreeNode {
  * public:
