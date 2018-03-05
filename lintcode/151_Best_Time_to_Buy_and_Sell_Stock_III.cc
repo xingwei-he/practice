59be5bd2ba33506a922d53ae390a9d21fc962ccf
xingwei.he <xingwei.he@corp.elong.com>
2018-03-05 16:02:47 +0800

    Best_Time_to_Buy_and_Sell_Stock

--- lintcode/151_Best_Time_to_Buy_and_Sell_Stock_III.cc
+++ lintcode/151_Best_Time_to_Buy_and_Sell_Stock_III.cc
@@ -33,8 +33,8 @@ public:
       return 0;
     }
     int n = prices.size();
-    vector<int> left_max_profit(n, 0);
-    vector<int> right_max_profit(n, 0);
+    vector<int> left_max_profit(n, 0);// 记录从第一天开始到第 i 天的最高收益（从前往后计算）
+    vector<int> right_max_profit(n, 0);// 记录从第 i+1 天到最后一天的最高收益（从后往前计算）
     int left_min = prices[0];
     int right_max = prices[n - 1];
     for (int i = 1; i < n; i++) {
@@ -51,6 +51,7 @@ public:
     }
     int max_profit = 0;
     for (int i = 0; i < n; i++) {
+      // 由于两次交易不能交叉覆盖，所以将第 i 天作为一个分割点，计算分割点处的和即为两次交易的最大收益
       if (max_profit < (left_max_profit[i] + right_max_profit[i])) {
 	max_profit = left_max_profit[i] + right_max_profit[i];
       }
@@ -60,7 +61,8 @@ public:
 };
 
 int main() {
-  int arr[] = {4,4,6,1,1,4,2,5};
+  //int arr[] = {4,4,6,1,1,4,2,5};
+  int arr[] = {1,7,15,6,57,32,76};
   vector<int> prices(arr, arr + sizeof(arr)/sizeof(int));
   Solution sl;
   int max_profit = sl.maxProfit(prices);
