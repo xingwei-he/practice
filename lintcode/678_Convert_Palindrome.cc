b86659b039b7674b4ca1f881b666f007a148fff2
xingwei.he <xingwei.he@corp.elong.com>
2018-03-07 19:01:57 +0800

    678_Convert_Palindrome

--- lintcode/678_Convert_Palindrome.cc
+++ lintcode/678_Convert_Palindrome.cc
@@ -9,6 +9,14 @@
 
 using namespace std;
 
+/**
+ * Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+ *
+ * Example
+ *   Given "aacecaaa", return "aaacecaaa".
+ *   Given "abcd", return "dcbabcd".
+ */
+
 class Solution {
 public:
   /*
