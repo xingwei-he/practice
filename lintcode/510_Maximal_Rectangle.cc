171f6a005d45bb86ae0572d91768b08657a5c9e0
xingwei.he <xingwei.he@corp.elong.com>
2018-03-19 17:07:02 +0800

    510_Maximal_Rectangle

--- lintcode/510_Maximal_Rectangle.cc
+++ lintcode/510_Maximal_Rectangle.cc
@@ -32,83 +32,86 @@ public:
    * @return: an integer
    */
   // DP
-  //int maximalRectangle(vector<vector<bool> > &matrix) {
-  TODO
-  int maximalRectangle(vector<vector<int> > &matrix) {
+  int maximalRectangle(vector<vector<bool> > &matrix) {
+    if (matrix.size() <= 0) {
+      return 0;
+    }
     int row_size = matrix.size();
     int col_size = matrix[0].size();
-    vector<vector<int> > left_border_matrix(row_size, vector<int>(col_size, 0));
-    vector<vector<int> > right_border_matrix(row_size, vector<int>(col_size, 0));
-    vector<vector<int> > height_matrix(row_size, vector<int>(col_size, 0));
+    int res = 0;
+    vector<int> left_border(col_size, INT_MIN);
+    vector<int> right_border(col_size, INT_MAX);
+    vector<int> height(col_size, 0);
     for (int i = 0; i < row_size; i++) {
+      int cur_left = 0;
+      int cur_right = col_size;
+      // left_border 左边界
       for (int j = 0; j < col_size; j++) {
 	if (true == matrix[i][j]) {
-	  if (0 == j) {
-	    left_border_matrix[i][j] = 1;
-	  } else {
-	    if (true == matrix[i][j - 1]) {
-	      left_border_matrix[i][j] = left_border_matrix[i][j - 1];
-	    } else {
-	      left_border_matrix[i][j] = 1;
-	    }
-	  }
+	  left_border[j] = max(left_border[j], cur_left);
+	} else {
+	  left_border[j] = 0;
+	  cur_left = j + 1;
 	}
       }
+      // right_border 右边界
       for (int j = col_size - 1; j >= 0; j--) {
 	if (true == matrix[i][j]) {
-	  if (col_size - 1 == j) {
-	    right_border_matrix[i][j] = col_size;
-	  } else {
-	    if (true == matrix[i][j + 1]) {
-	      right_border_matrix[i][j] = right_border_matrix[i][j + 1];
-	    } else {
-	      right_border_matrix[i][j] = j + 1;
-	    }
-	  }
+	  right_border[j] = min(right_border[j], cur_right);
+	} else {
+	  right_border[j] = col_size;
+	  cur_right = j;
 	}
       }
+      // height 高度
+      // 如果当前元素 matrix[i][j] 是 1，则高度为 matrix[i - 1][j] + 1
       for (int j = 0; j < col_size; j++) {
 	if (true == matrix[i][j]) {
-	  if (0 == j) {
-	    height_matrix[i][j] = 1;
-	  } else {
-	    if (true == matrix[i - 1][j]) {
-	      height_matrix[i][j] = height_matrix[i - 1][j] + 1;
-	    } else {
-	      height_matrix[i][j] = 1;
-	    }
-	  }
+	  height[j] += 1;
+	} else {
+	  height[j] = 0;
 	}
       }
+      // 计算结果
+      for (int j = 0; j < col_size; j++) {
+	res = max(res, (right_border[j] - left_border[j]) * height[j]);
+      }
+      cout << "l:";
+      debug1DVector<int>(left_border, 2);
+      cout << "r:";
+      debug1DVector<int>(right_border, 2);
+      cout << "h:";
+      debug1DVector<int>(height, 2);
+      cout << "* * * * * * * *" << endl;
     }
-    cout << "********" << endl;
-    debug2DVector<int>(left_border_matrix, 2);
-    cout << "********" << endl;
-    debug2DVector<int>(right_border_matrix, 2);
-    cout << "********" << endl;
-    debug2DVector<int>(height_matrix, 2);
-
-    return 0;
+    return res;
   }
 };
 
 int main() {
-  int r0[] = {1,1,0,0,1};
-  int r1[] = {0,1,0,0,1};
-  int r2[] = {0,0,1,1,1};
-  int r3[] = {0,0,1,1,1};
-  int r4[] = {0,0,0,0,1};
-  vector<int> row0(r0, r0 + sizeof(r0)/sizeof(int));
-  vector<int> row1(r1, r1 + sizeof(r1)/sizeof(int));
-  vector<int> row2(r2, r2 + sizeof(r2)/sizeof(int));
-  vector<int> row3(r3, r3 + sizeof(r3)/sizeof(int));
-  vector<int> row4(r4, r4 + sizeof(r4)/sizeof(int));
-  vector<vector<int> > matrix;
+  bool r0[] = {1,1,0,0,1};
+  bool r1[] = {0,1,0,0,1};
+  bool r2[] = {0,0,1,1,1};
+  bool r3[] = {0,0,1,1,1};
+  bool r4[] = {0,0,0,0,1};
+  vector<bool> row0(r0, r0 + sizeof(r0)/sizeof(bool));
+  vector<bool> row1(r1, r1 + sizeof(r1)/sizeof(bool));
+  vector<bool> row2(r2, r2 + sizeof(r2)/sizeof(bool));
+  vector<bool> row3(r3, r3 + sizeof(r3)/sizeof(bool));
+  vector<bool> row4(r4, r4 + sizeof(r4)/sizeof(bool));
+  vector<vector<bool> > matrix;
   matrix.push_back(row0);
   matrix.push_back(row1);
   matrix.push_back(row2);
   matrix.push_back(row3);
   matrix.push_back(row4);
+  /* case 2
+  bool r0[] = {1};
+  vector<bool> row0(r0, r0 + sizeof(r0)/sizeof(bool));
+  vector<vector<bool> > matrix;
+  matrix.push_back(row0);
+  */
+ 
   Solution sl;
   int res = sl.maximalRectangle(matrix);
   cout << "res:" << res << endl;
