7ce47ef2382ded14b11e292949c17ff0e97de4e0
xingwei <120977876@qq.com>
2018-03-21 00:05:21 +0800

    59_3Sum_Closest

---
+++ lintcode/59_3Sum_Closest.cc
@@ -0,0 +1,37 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:59_3Sum_Closest.cc
+ * Author:
+ * Mail:
+ * Created Time:2018年03月20日 星期二 23时56分36秒
+ ***************************************************/
+#include <iostream>
+#include "../include/base.h"
+
+using namespace std;
+
+/**
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers.
+ *
+ * Notice
+ *   You may assume that each input would have exactly one solution.
+ *
+ * Example
+ *   For example, given array S = [-1 2 1 -4], and target = 1. The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+
+class Solution {
+public:
+  /**
+   * @param numbers: Give an array numbers of n integer
+   * @param target: An integer
+   * @return: return the sum of the three integers, the sum closest target.
+   */
+  int threeSumClosest(vector<int> &numbers, int target) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
