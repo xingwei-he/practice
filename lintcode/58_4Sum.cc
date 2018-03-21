73bcccef34038fa7b859f4c700c11ff2b396b18d
xingwei.he <xingwei.he@corp.elong.com>
2018-03-21 19:02:10 +0800

    58_4Sum

---
+++ lintcode/58_4Sum.cc
@@ -0,0 +1,42 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:58_4Sum.cc
+ * Author:
+ * Mail:
+ * Created Time:星期三  3/21 18:47:49 2018
+ ***************************************************/
+#include <iostream>
+#include "../include/base.h"
+
+using namespace std;
+
+/**
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+ * Find all unique quadruplets in the array which gives the sum of target.
+ *
+ * Notice
+ *   Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+ *   The solution set must not contain duplicate quadruplets.
+ *
+ * Example
+ *   Given array S = {1 0 -1 0 -2 2}, and target = 0. A solution set is:
+ *   (-1, 0, 0, 1)
+ *   (-2, -1, 1, 2)
+ *   (-2, 0, 0, 2)
+ */
+
+class Solution {
+public:
+  /**
+   * @param numbers: Give an array
+   * @param target: An integer
+   * @return: Find all unique quadruplets in the array which gives the sum of zero
+   */
+  vector<vector<int>> fourSum(vector<int> &numbers, int target) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
