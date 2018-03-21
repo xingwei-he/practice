5f9a05cd106c07d81660746d0acf890268256c15
xingwei <120977876@qq.com>
2018-03-22 00:28:50 +0800

    117_Jump_Game_II

---
+++ lintcode/117_Jump_Game_II.cc
@@ -0,0 +1,36 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:117_Jump_Game_II.cc
+ * Author:
+ * Mail:
+ * Created Time:2018年03月22日 星期四 00时22分06秒
+ ***************************************************/
+#include <iostream>
+#include "../include/base.h"
+
+using namespace std;
+
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Your goal is to reach the last index in the minimum number of jumps.
+ *
+ * Example
+ *   Given array A = [2,3,1,1,4]
+ *   The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+ */
+
+class Solution {
+public:
+  /**
+   * @param A: A list of integers
+   * @return: An integer
+   */
+  int jump(vector<int> &A) {
+
+  }
+};
+
+int main() {
+  return 0;
+}
