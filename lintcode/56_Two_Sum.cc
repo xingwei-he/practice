a4be9b5d0e7e75a940e04cf2f1f2e6e1bd150ac2
xingwei.he <xingwei.he@corp.elong.com>
2018-03-21 18:39:41 +0800

    56_Two_Sum

--- lintcode/56_Two_Sum.cc
+++ lintcode/56_Two_Sum.cc
@@ -16,11 +16,11 @@ using namespace std;
  * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.
  *
  * Notice
- * You may assume that each input would have exactly one solution
+ *   You may assume that each input would have exactly one solution
  *
  * Example
- * numbers=[2, 7, 11, 15], target=9
- * return [1, 2]
+ *   numbers=[2, 7, 11, 15], target=9
+ *   return [1, 2]
  */
 class Solution {
 public:
