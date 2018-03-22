8f8c1b3d5367a7de56136276393a6d7b2bc20b60
xingwei.he <xingwei.he@corp.elong.com>
2018-03-22 19:53:20 +0800

    todo

---
+++ lintcode/683_Word_Break_III.cc
@@ -0,0 +1,44 @@
+/***************************************************
+ * Xingwei_He All Rights Reserved.
+ * File Name:683_Word_Break_III.cc
+ * Author:
+ * Mail:
+ * Created Time:星期四  3/22 15:54:21 2018
+ ***************************************************/
+#include <iostream>
+#include "../include/base.h"
+
+using namespace std;
+
+/**
+ * Give a dictionary of words and a sentence with all whitespace removed, return the number of sentences you can form by inserting whitespaces to the sentence so that each word can be found in the dictionary.
+ *
+ * Notice
+ *   Ignore case
+ *
+ * Example
+ *   Given a String CatMat
+ *   Given a dictionary ["Cat", "Mat", "Ca", "tM", "at", "C", "Dog", "og", "Do"]
+ *   return 3
+ *   we can form 3 sentences, as follows:
+ *   CatMat = Cat Mat
+ *   CatMat = Ca tM at
+ *   CatMat = C at Mat
+ */
+
+class Solution {
+public:
+  /*
+   * @param : A string
+   * @param : A set of word
+   * @return: the number of possible sentences.
+   */
+  int wordBreak3(string& s, unordered_set<string>& dict) {
+
+  }
+  void dfs()
+};
+
+int main() {
+  return 0;
+}
