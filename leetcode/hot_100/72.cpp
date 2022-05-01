8573d37cd56093f19d375128916794794e032d4d
hexingwei <120977876@qq.com>
2022-05-01 12:55:53 +0800

    add 72

---
+++ leetcode/hot_100/72.cpp
@@ -0,0 +1,63 @@
+/**************************************
+* He Xingwei, All Rights Reserved.
+* Filename:72.cpp
+* Date:2022-05-01 10:22:28
+* Description:
+**************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+    	int len1 = word1.size();
+    	int len2 = word2.size();
+    	if (0 == len1) {
+    		return len2;
+    	}
+    	if (0 == len2) {
+    		return len1;
+    	}
+    	// 动态规划辅助数组
+    	vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+    	// 初始化 dp
+    	for (int i = 0; i <= len1; i++) {
+    		dp[i][0] = i;
+    	}
+    	for (int j = 0; j <= len2; j++) {
+    		dp[0][j] = j;
+    	}
+    	// 填充 dp
+    	for (int i = 1; i <= len1; i++) {
+    		for (int j = 1; j <= len2; j++) {
+    			if (word1[i - 1] == word2[j - 1]) { // 注意，word中的坐标和dp中有漂移
+    				dp[i][j] = min(1 + min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+    			} else {
+    				dp[i][j] = 1 + min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+    			}
+    		}
+    	}
+        cout << word1 << endl;
+        cout << word2 << endl;
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                cout << dp[i][j] << "\t";
+            }
+            cout << endl;
+        }
+    	return dp[len1][len2];
+    }
+};
+
+int main() {
+	string word1 = "horse";
+	string word2 = "ros";
+	Solution s;
+	int res = s.minDistance(word1, word2);
+	cout << res << endl;
+	return 0;
+}
